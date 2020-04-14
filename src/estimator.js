9ccd2609d3f93892681c05ec1a1c2d573978a142
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-04-14 16:24:47 +0100

    resfactoring

--- src/estimator.js
+++ src/estimator.js
@@ -1,55 +1,47 @@
 const covid19ImpactEstimator = (data) => {
+  let impact = {};
+  let severeImpact = {};
   let factor;
-  let num;
   const days = data.periodType.toLowerCase();
 
 
   // converts to days
   if (days === 'days') {
     data.timeToElapse *= 1;
+    factor = Math.trunc(2 ** (data.timeToElapse / 3));
   } else if (days === 'weeks') {
     data.timeToElapse *= 7;
+    factor = Math.trunc(2 ** (data.timeToElapse / 3));
   } else if (days === 'months') {
     data.timeToElapse *= 30;
+    factor = Math.trunc(2 ** (data.timeToElapse / 3));
   }
-  // double report every 3 days
-  if (days === 'days' && data.timeToElapse < 3) {
-    factor = 1;
-    num = 1;
-  } else if (data.timeToElapse >= 3) {
-    factor = Math.floor(data.timeToElapse / 3);
-    num = 2;
-  }
-
-  // doubles currently infected every 3days
-  const outputData = {
-    data,
-    impact: {
-      currentlyInfected: data.reportedCases * 10,
-      infectionsByRequestedTime: this.currentlyInfected * (num ** factor),
-      severeCasesByRequestedTime: this.infectionsByRequestedTime * 0.15,
-      hospitalBedsByRequestedTime: data.totalHospitalBeds * 0.35,
-      casesForICUByRequestedTime: this.infectionsByRequestedTime * 0.05,
-      casesForVentilatorsByRequestedTime: this.infectionsByRequestedTime * 0.02,
-      dollarsInFlight: (this.infectionsByRequestedTime * data.avgDailyIncomePopulation)
-      * data.avgDailyIncomeInUSD * data.timeToElapse
-    },
-    //  severe case estimation
-    severeImpact: {
-      currentlyInfected: data.reportedCases * 50,
-      infectionsByRequestedTime: this.currentlyInfected * (num ** factor),
-      severeCasesByRequestedTime: this.infectionsByRequestedTime * 0.15,
-      hospitalBedsByRequestedTime: data.totalHospitalBeds * 0.35,
-      casesForICUByRequestedTime: this.infectionsByRequestedTime * 0.05,
-      casesForVentilatorsByRequestedTime: this.infectionsByRequestedTime * 0.02,
-      dollarsInFlight: (this.infectionsByRequestedTime * data.avgDailyIncomePopulation)
-       * data.avgDailyIncomeInUSD * data.timeToElapse
-    }
-
+  impact = {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: this.currentlyInfected * (factor),
+    severeCasesByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.15),
+    hospitalBedsByRequestedTime: Math.trunc(data.totalHospitalBeds * 0.35),
+    casesForICUByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.05),
+    casesForVentilatorsByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.02),
+    dollarsInFlight: Math.trunc((this.infectionsByRequestedTime * data.avgDailyIncomePopulation
+      * data.avgDailyIncomeInUSD) / data.timeToElapse)
+  };
+  severeImpact = {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: this.currentlyInfected * (factor),
+    severeCasesByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.15),
+    hospitalBedsByRequestedTime: Math.trunc(data.totalHospitalBeds * 0.35),
+    casesForICUByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.05),
+    casesForVentilatorsByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.02),
+    dollarsInFlight: Math.trunc((this.infectionsByRequestedTime * data.avgDailyIncomePopulation
+      * data.avgDailyIncomeInUSD) / data.timeToElapse)
   };
 
-
-  return outputData;
+  return {
+    data,
+    impact,
+    severeImpact
+  };
 };
 
 export default covid19ImpactEstimator;
